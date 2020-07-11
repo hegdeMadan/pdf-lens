8e0d0f5f921b84369b9cde17d263edb881d3b95c
hegdeMadan <madanhegde99@gmail.com>
2020-07-11 12:05:38 +0530

    document scanner

--- src/dashboard/home.tsx
+++ src/dashboard/home.tsx
@@ -1,34 +1,85 @@
-import React from 'react';
-import { View, Text } from 'react-native'
-import RNImageToPdf from 'react-native-image-to-pdf';
+import React, { useRef, useState, useEffect } from "react"
+import { View, StyleSheet, Text, TouchableOpacity, Image, Platform } from "react-native"
+import Permissions from 'react-native-permissions';
+import PDFScanner from "@woonivers/react-native-document-scanner"
 
-const Home = () => {
-  const myAsyncPDFFunction = async () => {
-    try {
-      const options = {
-        imagePaths: ['/path/to/image1.png','/path/to/image2.png'],
-        name: 'PDFName',
-        maxSize: { // optional maximum image dimension - larger images will be resized
-          width: 900,
-          height: Math.round((1080 / 720) * 900),
-        },
-        quality: 0.7, // optional compression paramter
-      };
-      const pdf = await RNImageToPdf.createPDFbyImages(options);
-      
-      console.log(pdf.filePath);
-    } catch(e) {
-      console.log(e);
+export default function App() {
+  const pdfScannerElement = useRef(null)
+  const [data, setData] = useState({})
+  const [allowed, setAllowed] = useState(false)
+
+  useEffect(() => {
+    async function requestCamera() {
+      const result = await Permissions.request(Platform.OS === "android" ? "android.permission.CAMERA" : "ios.permission.CAMERA")
+      if (result === "granted") setAllowed(true)
     }
+    requestCamera()
+  }, [])
+
+  function handleOnPressRetry() {
+    setData({})
+  }
+  function handleOnPress() {
+    pdfScannerElement.current.capture()
+  }
+  if (!allowed) {
+    console.log("You must accept camera permission")
+    return (<View style={styles.permissions}>
+      <Text>You must accept camera permission</Text>
+    </View>)
+  }
+  if (data.croppedImage) {
+    console.log("data", data)
+    return (
+      <React.Fragment>
+        <Image source={{ uri: data.croppedImage }} style={styles.preview} />
+        <TouchableOpacity onPress={handleOnPressRetry} style={styles.button}>
+          <Text style={styles.buttonText}>Retry</Text>
+        </TouchableOpacity>
+      </React.Fragment>
+    )
   }
   return (
-    <View>
-      <Text>
-        pdf
-      </Text>
-      
-    </View>
-  );
+    <React.Fragment>
+      <PDFScanner
+        ref={pdfScannerElement}
+        style={styles.scanner}
+        onPictureTaken={setData}
+        overlayColor="rgba(255,130,0, 0.7)"
+        enableTorch={false}
+        quality={0.5}
+        detectionCountBeforeCapture={5}
+        detectionRefreshRateInMS={50}
+      />
+      <TouchableOpacity onPress={handleOnPress} style={styles.button}>
+        <Text style={styles.buttonText}>Take picture</Text>
+      </TouchableOpacity>
+    </React.Fragment>
+  )
 }
 
-export default Home;
+const styles = StyleSheet.create({
+  scanner: {
+    flex: 1,
+    aspectRatio: undefined
+  },
+  button: {
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 32,
+  },
+  buttonText: {
+    backgroundColor: "rgba(245, 252, 255, 0.7)",
+    fontSize: 32,
+  },
+  preview: {
+    flex: 1,
+    width: "100%",
+    resizeMode: "cover",
+  },
+  permissions: {
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
